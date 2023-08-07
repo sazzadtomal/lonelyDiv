904edfddd14ebe26ef913775c9f4ab504f9febce
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2023-08-07 19:35:58 +0600

    developed landing page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import AnimatedHeader from './components/AnimatedHeader/AnimatedHeader';
 import AnimatedBody from './components/AnimatedBody/AnimatedBody';
 const App = () => {
   return (
-    <div className='flex justify-center items-center bg-slate-900 w-full h-screen fixed'>
+    <div className='flex flex-col justify-center items-center bg-slate-900 w-screen h-screen'>
          <AnimatedHeader/>
-         <LonelyDiv/>
-         <AnimatedBody/>
+         <div className='m-4 relative flex justify-between h-2/3 md:w-1/2 lg:w-1/3'>
+               <LonelyDiv/>
+               <AnimatedBody/>
+          </div>
+          <button className= 'cursor-pointer hover:text-cyan-500 bg-white rounded-xl md:w-1/6 w-1/3 p-3'>ClickMe</button>
          
     </div>
   )
--- src/components/AnimatedBody/AnimatedBody.js
+++ src/components/AnimatedBody/AnimatedBody.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import "./AnimatedBody.css"
 const AnimatedBody = () => {
   return (
-    <h2 className='m-5 text-3xl w-52 text-white font-semibold'>I used to be a lonely div. Just written in Html and some styling with CSS</h2>
+
+      <div className='flex  flex-1 items-center overflow-hidden'>
+        <p className='ml-4 bodyOpacity text-center text-lg md:text-3xl  text-white '>I used to be a lonely div. Just written in Html and some styling with CSS..</p>
+      </div>
+
   )
 }
 
